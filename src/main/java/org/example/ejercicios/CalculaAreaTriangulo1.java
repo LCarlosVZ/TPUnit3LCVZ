2f9470c03ab8a3813751f90657d5f46e46842ee5
LCarlosVZ <lcarlosvazap@gmail.com>
2023-10-21 23:50:26 -0500

    Cambiar nombres de las variables a nombres descriptivos

--- src/main/java/org/example/ejercicios/CalculaAreaTriangulo1.java
+++ src/main/java/org/example/ejercicios/CalculaAreaTriangulo1.java
@@ -4,9 +4,9 @@ public class CalculaAreaTriangulo1 {
 	public static void main(String[] args) {
 		CaracteristicasDelTriangulo triangulo = new CaracteristicasDelTriangulo("rojo", 5.0, 4.0);
 		double valor_area_triangulo = 0D;
-		double b = triangulo.baseDelTriangulo();
-		double h = triangulo.alturaDelTriangulo();
-		valor_area_triangulo =  0.5 * b * h;
+		double baseTriangulo = triangulo.baseDelTriangulo();
+		double alturaTriangulo = triangulo.alturaDelTriangulo();
+		valor_area_triangulo =  0.5 * baseTriangulo * alturaTriangulo;
 		System.out.println("Altura del triángulo: " + triangulo.alturaDelTriangulo());
 		System.out.println("Base del triángulo: " + triangulo.baseDelTriangulo());
 		System.out.println("Área del triángulo: " + valor_area_triangulo);
@@ -16,9 +16,12 @@ public class CalculaAreaTriangulo1 {
 }
 
 /*
-CAMBIÉ EN EL RECORD:
+CAMBIÉ EN EL RECORD POR BUENAS PRACTICAS VARIAS PALABRAS A PALABRAS MÁS DESCRIPTIVOS:
   Triangulo a CaracteristicasDelTriangulo
   c a colorDelTriangulo
   b a baseDelTriangulo
   h a alturaDelTriangulo
+CAMBIÉ EL NOMBRE DE ALGUNAS VARIABLES A UNAS MÁS DESCRIPTIVAS
+  b a baseTriangulo
+  h a alturaTriangulo
  */
