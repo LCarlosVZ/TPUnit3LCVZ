b043b4aa9312d862c3f0e7985b0ed8f0fb5d3db0
LCarlosVZ <lcarlosvazap@gmail.com>
2023-10-21 23:46:08 -0500

    Cambiar nombres del record por nombres descriptivos

--- src/main/java/org/example/ejercicios/CalculaAreaTriangulo1.java
+++ src/main/java/org/example/ejercicios/CalculaAreaTriangulo1.java
@@ -2,15 +2,23 @@ package org.example.ejercicios;
 
 public class CalculaAreaTriangulo1 {
 	public static void main(String[] args) {
-		Triangulo triangulo = new Triangulo("rojo", 5.0, 4.0);
+		CaracteristicasDelTriangulo triangulo = new CaracteristicasDelTriangulo("rojo", 5.0, 4.0);
 		double valor_area_triangulo = 0D;
-		double b = triangulo.b();
-		double h = triangulo.h();
+		double b = triangulo.baseDelTriangulo();
+		double h = triangulo.alturaDelTriangulo();
 		valor_area_triangulo =  0.5 * b * h;
-		System.out.println("Altura del triángulo: " + triangulo.h());
-		System.out.println("Base del triángulo: " + triangulo.b());
+		System.out.println("Altura del triángulo: " + triangulo.alturaDelTriangulo());
+		System.out.println("Base del triángulo: " + triangulo.baseDelTriangulo());
 		System.out.println("Área del triángulo: " + valor_area_triangulo);
 	}
 
-	record Triangulo(String c, double b, double h) {}
+	record CaracteristicasDelTriangulo(String colorDelTriangulo, double baseDelTriangulo, double alturaDelTriangulo) {}
 }
+
+/*
+CAMBIÉ EN EL RECORD:
+  Triangulo a CaracteristicasDelTriangulo
+  c a colorDelTriangulo
+  b a baseDelTriangulo
+  h a alturaDelTriangulo
+ */
