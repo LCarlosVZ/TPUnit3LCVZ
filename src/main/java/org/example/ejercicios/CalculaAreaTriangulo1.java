0d9f94ca4bbd8b5433e196043f8a6b420c18629f
LCarlosVZ <lcarlosvazap@gmail.com>
2023-10-22 01:24:24 -0500

    Organicé  en en el inicio de los programas la descripción de los cambios realizados

--- src/main/java/org/example/ejercicios/CalculaAreaTriangulo1.java
+++ src/main/java/org/example/ejercicios/CalculaAreaTriangulo1.java
@@ -1,5 +1,16 @@
 package org.example.ejercicios;
 
+/*
+CAMBIÉ EN EL RECORD POR BUENAS PRACTICAS VARIAS PALABRAS A PALABRAS MÁS DESCRIPTIVOS:
+  Triangulo a CaracteristicasDelTriangulo
+  c a colorDelTriangulo
+  b a baseDelTriangulo
+  h a alturaDelTriangulo
+CAMBIÉ EL NOMBRE DE ALGUNAS VARIABLES A UNAS MÁS DESCRIPTIVAS
+  b a baseTriangulo
+  h a alturaTriangulo
+ */
+
 public class CalculaAreaTriangulo1 {
 	public static void main(String[] args) {
 		CaracteristicasDelTriangulo triangulo = new CaracteristicasDelTriangulo("rojo", 5.0, 4.0);
@@ -15,13 +26,3 @@ public class CalculaAreaTriangulo1 {
 	record CaracteristicasDelTriangulo(String colorDelTriangulo, double baseDelTriangulo, double alturaDelTriangulo) {}
 }
 
-/*
-CAMBIÉ EN EL RECORD POR BUENAS PRACTICAS VARIAS PALABRAS A PALABRAS MÁS DESCRIPTIVOS:
-  Triangulo a CaracteristicasDelTriangulo
-  c a colorDelTriangulo
-  b a baseDelTriangulo
-  h a alturaDelTriangulo
-CAMBIÉ EL NOMBRE DE ALGUNAS VARIABLES A UNAS MÁS DESCRIPTIVAS
-  b a baseTriangulo
-  h a alturaTriangulo
- */
