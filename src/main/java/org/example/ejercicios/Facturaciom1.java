5062d98f6d64e62e2b82ecf2234b4e7eed5bcb56
LCarlosVZ <lcarlosvazap@gmail.com>
2023-10-21 23:59:01 -0500

    Cambiar nombres en el record y aplicar CamelCase

--- src/main/java/org/example/ejercicios/Facturaciom1.java
+++ src/main/java/org/example/ejercicios/Facturaciom1.java
@@ -2,20 +2,28 @@ package org.example.ejercicios;
 
 import java.util.List;
 
+/*
+CAMBIÉ NOMBRES EN EL RECORD PARA SER MÁS DESCRIPTIVO
+ Factura a DatosFactura
+ cliente a nombreCliente
+APLIQUÉ CAMELCASE EN:
+ fechafactura a fechaFactura
+ */
+
 public class Facturaciom1 {
-	record Factura(String cliente, String fechafactura, List<Double> precioProducto) {
+	record DatosFactura(String nombreCliente, String fechaFactura, List<Double> precioProducto) {
 	}
 
 	public static void main(String[] args) {
-		Factura factura1 = new Factura("Maria Sanabria", "Julio 1, 1999", List.of(4500D, 5000D, 2500D));
-		Factura factura2 = new Factura("Juan Perez", "Agosto 15, 2000", List.of(3500D, 6000D, 2800D));
+		DatosFactura factura1 = new DatosFactura("Maria Sanabria", "Julio 1, 1999", List.of(4500D, 5000D, 2500D));
+		DatosFactura factura2 = new DatosFactura("Juan Perez", "Agosto 15, 2000", List.of(3500D, 6000D, 2800D));
 
 		double totalValorFact1 = 0;
 		for (Double precio : factura1.precioProducto) {
 			totalValorFact1 += precio;
 		}
 		System.out.println("Factura 1" );
-		System.out.println("Cliente: " + factura1.cliente());
+		System.out.println("Cliente: " + factura1.nombreCliente());
 		System.out.println("Total: " + totalValorFact1);
 
 
@@ -24,7 +32,7 @@ public class Facturaciom1 {
 			totalValorFact2 += precio;
 		}
 		System.out.println("Factura 2" );
-		System.out.println("Cliente: " + factura2.cliente());
+		System.out.println("Cliente: " + factura2.nombreCliente());
 		System.out.println("Total: " + totalValorFact2);
 	}
 
