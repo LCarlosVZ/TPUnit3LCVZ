abedde1c4924d06a4d28dd00249ad04e2bf737a5
LCarlosVZ <lcarlosvazap@gmail.com>
2023-10-22 01:21:03 -0500

    Se eliminó el código anterior sobre el que se basó la organización del nuevo

--- src/main/java/org/example/ejercicios/Facturaciom1.java
+++ src/main/java/org/example/ejercicios/Facturaciom1.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 /*
 CAMBIÉ NOMBRES EN EL RECORD PARA SER MÁS DESCRIPTIVO:
- Factura a DatosFactura
- cliente a nombreCliente
+  Factura a DatosFactura
+  cliente a nombreCliente
 APLIQUÉ CAMELCASE EN:
- fechafactura a fechaFactura
-CREÉ UN MÉTODO PARA MANEJAR LOS DATOS EN UNA MISMA COLECCIÓN DE OBJETOS.
-CREÉ UN MÉTODO PARA MOSTRAR LOS DATOS DE FORMA INDIVIDUAL DESDE UNA MISMA COLECCIÓN DE OBJETOS.
+  fechafactura a fechaFactura
+
+DIVIDÍ LAS TAREAS EN MÉTODOS
+  CREÉ UN MÉTODO PARA MANEJAR LOS DATOS EN UNA MISMA COLECCIÓN DE OBJETOS.
+  CREÉ UN MÉTODO PARA MOSTRAR LOS DATOS DE FORMA INDIVIDUAL DESDE UNA MISMA COLECCIÓN DE OBJETOS.
 
  */
 
@@ -19,40 +21,17 @@ public class Facturaciom1 {
     }
 
     public static void main(String[] args) {
-
-    /*
-        DatosFactura factura1 = new DatosFactura("Maria Sanabria", "Julio 1, 1999", List.of(4500D, 5000D, 2500D));
-        DatosFactura factura2 = new DatosFactura("Juan Perez", "Agosto 15, 2000", List.of(3500D, 6000D, 2800D));
-
-        double totalValorFact1 = 0;
-        for (Double precio : factura1.precioProducto) {
-            totalValorFact1 += precio;
-        }
-        System.out.println("Factura 1");
-        System.out.println("Cliente: " + factura1.nombreCliente());
-        System.out.println("Total: " + totalValorFact1);
-
-
-        double totalValorFact2 = 0;
-        for (Double precio : factura2.precioProducto) {
-            totalValorFact2 += precio;
-        }
-        System.out.println("Factura 2");
-        System.out.println("Cliente: " + factura2.nombreCliente());
-        System.out.println("Total: " + totalValorFact2);
-
-     */
+        mostrarDatosFacturas(retornarDatosDeFacturas());
     }
 
-
-    public ArrayList<DatosFactura> retornarDatosDeFacturas() {
+    public static ArrayList<DatosFactura> retornarDatosDeFacturas() {
         ArrayList<DatosFactura> datosParaFactura = new ArrayList<>();
         datosParaFactura.add(new DatosFactura("Maria Sanabria", "Julio 1, 1999", List.of(4500D, 5000D, 2500D)));
         datosParaFactura.add(new DatosFactura("Juan Perez", "Agosto 15, 2000", List.of(3500D, 6000D, 2800D)));
         return datosParaFactura;
     }
 
-    public void mostrarDatosFacturas(ArrayList<DatosFactura> mostrarDatos) {
+    public static void mostrarDatosFacturas(ArrayList<DatosFactura> mostrarDatos) {
         Double totalValorFactura = 0D;
         for (int i = 0; i <= mostrarDatos.size(); i++) {
             for (Double precio : mostrarDatos.get(i).precioProducto) {
